9969f9567e6e06017d13cf96857576a4117fb345
rtmigo <art.galkin@gmail.com>
2021-09-23 05:32:26 +0300

    upd

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # [img2texture](https://github.com/rtmigo/img2texture_py#readme) (DRAFT)
 
 Command line utility for converting images to seamless tiles.
+
 The resulting tiles can be used as textures in games, compositing and 3D modeling applications, etc.
 
 # Install
@@ -15,3 +16,14 @@ $ pip3 install https://github.com/rtmigo/img2texture_py
 $ img2texture /path/to/source.jpg /path/to/seamless_result.jpg 
 ```
 
+### Source image
+![Text](docs/1_orion_src.jpg)
+
+### Source tiled 
+![Text](docs/1_orion_src_2x2.jpg)
+
+### Converted image
+![Text](docs/2_orion_seamless.jpg)
+
+### Converted tiled
+![Text](docs/2_orion_seamless.jpg2x2.jpg)
--- img2texture/_cli.py
+++ img2texture/_cli.py
@@ -1,13 +1,21 @@
 import argparse
+import os
 import sys
+from enum import Enum
 from pathlib import Path
 
 from img2texture import img2tex
 from ._constants import __version__ as version, __copyright__ as copyright
+from ._tiling import tile
 
 
 def print_version():
     print(f'img2texture {version} {copyright}')
+
+
+class Mode(Enum):
+    both = "both"
+    none = "none"
 
 
 class ParsedArgs:
@@ -25,6 +33,14 @@ class ParsedArgs:
                             help="Path of source image file.")
         parser.add_argument("target",
                             help="Path of target (tile) file.")
+        parser.add_argument("--tile",
+                            action='store_true',
+                            default=False,
+                            help="Also make 2x2 tile.")
+        parser.add_argument("--mode",
+                            choices=[str(m.value) for m in Mode],
+                            default=Mode.both)
+
         parser.add_argument('--version',
                             action='store_true',
                             default=False,
@@ -44,7 +60,28 @@ class ParsedArgs:
     def mix(self) -> float:
         return self._parsed.mix
 
+    @property
+    def mode(self) -> Mode:
+        return Mode(self._parsed.mode)
+
+    @property
+    def tile(self) -> bool:
+        return self._parsed.tile
+
+
+def tile_filename(texture: Path) -> Path:
+    basename = texture.name
+    basename = basename.rpartition('.')[0]
+    basename += "_2x2.jpg"
+    return texture.parent / basename
+
 
 def cli():
     args = ParsedArgs()
     img2tex(args.source, args.target, pct=args.mix)
+    if args.tile:
+        tile_src = args.target if args.mode != Mode.none else args.source
+        tile_fn = tile_filename(tile_src)
+        if tile_fn.exists():
+            os.remove(tile_fn)
+        tile(tile_src, tile_fn, horizontal=2, vertical=2)
