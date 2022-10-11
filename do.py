cf4d4d4b0b77ffd2b2f06d2f22bbe9f3f6fd5535
Artem IG <ortemeo@gmail.com>
2022-10-11 21:08:34 +0300

    building exe

---
+++ do.py
@@ -0,0 +1,92 @@
+import datetime
+import os
+import re
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+import PyInstaller.__main__ as compile
+import click
+import neatest
+from chkpkg import Package
+
+
+@click.group()
+def app():
+    pass
+
+
+@app.command()
+def test():
+    _test()
+
+
+def _test():
+    neatest.run(warnings=neatest.Warnings.fail)
+
+
+@app.command()
+def test_pkg():
+    with Package() as pkg:
+        pkg.run_shell_code('img2texture --version')
+    print("\nPackage is OK!")
+
+
+@app.command()
+def run():
+    subprocess.call([sys.executable, "-m", "img2texture"])
+
+
+@app.command()
+def lint():
+    _lint()
+
+
+def _lint():
+    print("Running mypy...")
+    if subprocess.call(['mypy', 'img2texture',
+                        '--ignore-missing-imports']) != 0:
+        exit(1)
+
+
+def _replace_build_date(fn: Path):
+    now = datetime.datetime.now().isoformat(sep=" ", timespec="seconds")
+    text = fn.read_text()
+    new_text = re.sub(
+        r'__build_timestamp__.+',
+        f'__build_timestamp__ = "{now}"',
+        text)
+    print(new_text)
+    assert new_text != text, "timestamp not changed"
+    fn.write_text(new_text)
+
+
+@app.command()
+def build():
+    name = "img2texture"
+    project_dir = Path(__file__).parent
+
+    _test()
+    _lint()
+
+    _replace_build_date(Path("img2texture/_constants.py"))
+
+    compile.run([
+        "--clean", "--onefile", "-y",
+        "--collect-all", "img2texture",
+        "--name", name, "_run.py"
+    ])
+
+    exe = project_dir / "dist" / name
+    print()
+    print(f"Created {exe}")
+    print(f"Exe size: {exe.stat().st_size / 1024 / 1024:.0f} MiB")
+
+    os.remove(project_dir / "img2texture.spec")
+    shutil.rmtree(project_dir / "build")
+
+
+
+if __name__ == "__main__":
+    app()
--- img2texture/_constants.py
+++ img2texture/_constants.py
@@ -1,4 +1,5 @@
 __version__ = "0.1.1"
 
-__copyright__ = "(c) 2021 Artsiom iG <github.com/rtmigo>"
+__copyright__ = "(c) 2021 Artsiom iG (github.com/rtmigo)"
 __license__ = "MIT"
+__build_timestamp__ = "2022-10-11 21:05:06"
