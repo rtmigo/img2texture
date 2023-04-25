6f4aaae7d37c9f229eec9008ab93960af6890147
Artem IG <ortemeo@gmail.com>
2023-04-26 02:54:06 +0300

    !!release !!master !!pypi

--- img2texture/_cli.py
+++ img2texture/_cli.py
@@ -118,9 +118,10 @@ def tile_filename(texture: Path) -> Path:
 def cli():
     args = ParsedArgs()
 
-    # preventing "Image.DecompressionBombError: Image size (324000000 pixels)
-    # exceeds limit of 178956970 pixels, could be decompression bomb DOS attack".
-    # But in case of CLI we are not expecting attacks. Just opening large files
+    # preventing exception "Image.DecompressionBombError: Image size (324000000
+    # pixels) exceeds limit of 178956970 pixels, could be decompression bomb DOS
+    # attack". In case of CLI we are not expecting attacks. Just processing
+    # large files
     Image.MAX_IMAGE_PIXELS = None
 
     try:
