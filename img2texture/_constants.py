7fce5fd8806f9f2a3dcc9d479b6148b38e47c78a
Artem IG <ortemeo@gmail.com>
2023-10-06 19:37:00 +0300

    releasing 1.1.2 gha:release gha:pypi gha:master

--- img2texture/_constants.py
+++ img2texture/_constants.py
@@ -2,5 +2,5 @@ __version__ = "1.1.2"
 
 __copyright__ = "(c) Arte:m iG <ortemeo@gmail.com>"
 __license__ = "MIT"
-__build_timestamp__ = "2022-10-12 05:30:19"
+__build_timestamp__ = "2022-10-12 05:30:20"
 
